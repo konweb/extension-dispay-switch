b99b029d4eaee9c5fa67556b483078549d43cdf1
mb_kondou <kondou@maboroshi.biz>
2015-02-20 00:23:26 +0900

    Click change icon.

--- bk.js
+++ bk.js
@@ -1,5 +1,16 @@
+var count = 2;
+function iconChange(tab){
+	chrome.browserAction.setIcon({path:'icon_0' + count + '.png', tabId:tab.id});
+	if(count === 2){
+		count = 1;
+	}else{
+		count = 2;
+	}
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.executeScript(tab.id, {file: "jquery_2.1.3.min.js"}, function(){
 		chrome.tabs.executeScript(tab.id, {file: "app.js"});
 	});
+	iconChange(tab);
 });
